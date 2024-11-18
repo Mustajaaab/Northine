f3b49ab216e4cb1691111c8fdbb440abe06e7c8b
Mustajab <kashifalykhan1999@gmail.com>
2024-11-18 12:22:03 +0500

    sec 2 done

---
+++ src/components/sec2/index.jsx
@@ -0,0 +1,217 @@
+import Photo1 from '../../assets/images/1.png'
+import Photo2 from '../../assets/images/2.png'
+import Photo3 from '../../assets/images/3.png'
+import Photo4 from '../../assets/images/4.png'
+import Photo5 from '../../assets/images/5.png'
+import Photo6 from '../../assets/images/6.png'
+import Shades from '../../assets/images/white-shades.png'
+import Cloud from '../../assets/images/cloud.png';
+import Bell from '../../assets/images/bell.png'
+import Setting from '../../assets/images/setting.png'
+import File from '../../assets/images/file.png'
+import Spark from '../../assets/images/spark.png'
+import Save from '../../assets/images/save.png'
+import Motion from '../../assets/images/motion.png'
+import Phones from '../../assets/images/phones.png'
+
+function Sec2() {
+    return (
+        <>
+        <div className='flex justify-center items-center gap-[88px] py-[92px]'>
+            <img src={Photo1} alt="" className='w-[150px] h-[40px]'/>
+            <img src={Photo2} alt="" className='w-[150px] h-[40px]'/>
+            <img src={Photo3} alt="" className='w-[150px] h-[40px]'/>
+            <img src={Photo4} alt="" className='w-[150px] h-[40px]'/>
+            <img src={Photo5} alt="" className='w-[150px] h-[40px]'/>
+            <img src={Photo6} alt="" className='w-[150px] h-[40px]'/>
+        </div>
+        <div className='w-[69%] flex mx-auto pb-3 gap-1'>
+            <div className='w-[2%] h-[2px] bg-[#F57C00]'></div>
+            <div className='w-[96%] h-[2px] bg-[#121820]/10'></div>
+            <div className='w-[2%] h-[2px] bg-[#F57C00]'></div>
+        </div>
+        <div className='w-[55%] mx-auto flex justify-end'>
+        <img src={Shades} alt="" className='w-[203px] h-[140px]'/>
+        </div>
+        <div className='w-[70%] mx-auto'>
+            <h1 className='font-syne font-semibold text-[42px] leading-[58px]'>How We Can <span className='text-[#F57C00]'>Help You</span></h1>
+            <div className='flex mt-14'>
+                <p className='font-syne font-semibold text-[22px] leading-8 w-[50%]'>IT Services</p>
+                <p className='font-syne font-semibold text-[22px] leading-8'>IT Solutions</p>
+            </div>
+            <div className="flex mt-[60px] w-full h-5">
+                <div className="flex gap-1 w-full">
+                    <div className="w-[2%] h-[2px] bg-[#F57C00]"></div>
+                    <div className="w-[96%] h-[2px] bg-gray-300"></div>
+                </div>
+                <div className="flex gap-1 w-full">
+                    <div className="w-[2%] h-[2px] bg-[#F57C00]"></div>
+                    <div className="w-[96%] h-[2px] bg-gray-300"></div>
+                </div>
+            </div>
+            <div className='flex mt-[30px]'>
+                <div className='flex gap-[121px] w-[50%]'>
+                    <img src={Cloud} alt="" className='w-[45px] h-[45px] ml-[31px]'/>
+                    <div>
+                        <div className='flex gap-3'>
+                            <div className='text-[#F57C00] font-semibold text-lg font-syne'>01</div>
+                            <h2 className='font-semibold font-syne text-lg'>Dedicated Team</h2>
+                        </div>
+                        <p className='text-[#121820]/40 font-syne text-[15px] leading-[25.5px] mt-[30px]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, <br />
+                        sed do eiusmod tempor incididunt ut labore et dolore <br />
+                        magna aliqua.</p>
+                    </div>
+                </div>
+
+                <div className='flex gap-[121px]'>
+                    <img src={Setting} alt="" className='w-[45px] h-[45px] ml-[31px]'/>
+                    <div>
+                        <div className='flex gap-3'>
+                            <div className='text-[#F57C00] font-semibold text-lg font-syne'>01</div>
+                            <h2 className='font-semibold font-syne text-lg'>Blockchain</h2>
+                        </div>
+                        <p className='text-[#121820]/40 font-syne text-[15px] leading-[25.5px] mt-[30px]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, <br />
+                        sed do eiusmod tempor incididunt ut labore et dolore <br />
+                        magna aliqua.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="flex mt-[60px] w-full h-5">
+                <div className="flex gap-1 w-full">
+                    <div className="w-[2%] h-[2px] bg-[#F57C00]"></div>
+                    <div className="w-[96%] h-[2px] bg-gray-300"></div>
+                </div>
+                <div className="flex gap-1 w-full">
+                    <div className="w-[2%] h-[2px] bg-[#F57C00]"></div>
+                    <div className="w-[96%] h-[2px] bg-gray-300"></div>
+                </div>
+            </div>
+            <div className='flex mt-[30px]'>
+                <div className='flex gap-[121px] w-[50%]'>
+                    <img src={Bell} alt="" className='w-[45px] h-[45px] ml-[31px]'/>
+                    <div>
+                        <div className='flex gap-3'>
+                            <div className='text-[#F57C00] font-semibold text-lg font-syne'>02</div>
+                            <h2 className='font-semibold font-syne text-lg'>QA and Testing</h2>
+                        </div>
+                        <p className='text-[#121820]/40 font-syne text-[15px] leading-[25.5px] mt-[30px]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, <br />
+                        sed do eiusmod tempor incididunt ut labore et dolore <br />
+                        magna aliqua.</p>
+                    </div>
+                </div>
+
+                <div className='flex gap-[121px]'>
+                    <img src={Spark} alt="" className='w-[45px] h-[45px] ml-[31px]'/>
+                    <div>
+                        <div className='flex gap-3'>
+                            <div className='text-[#F57C00] font-semibold text-lg font-syne'>02</div>
+                            <h2 className='font-semibold font-syne text-lg'>Artificial Intelligence</h2>
+                        </div>
+                        <p className='text-[#121820]/40 font-syne text-[15px] leading-[25.5px] mt-[30px]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, <br />
+                        sed do eiusmod tempor incididunt ut labore et dolore <br />
+                        magna aliqua.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="flex mt-[60px] w-full h-5">
+                <div className="flex gap-1 w-full">
+                    <div className="w-[2%] h-[2px] bg-[#F57C00]"></div>
+                    <div className="w-[96%] h-[2px] bg-gray-300"></div>
+                </div>
+                <div className="flex gap-1 w-full">
+                    <div className="w-[2%] h-[2px] bg-[#F57C00]"></div>
+                    <div className="w-[96%] h-[2px] bg-gray-300"></div>
+                </div>
+            </div>
+            <div className='flex mt-[30px]'>
+                <div className='flex gap-[121px] w-[50%]'>
+                    <img src={File} alt="" className='w-[45px] h-[45px] ml-[31px]'/>
+                    <div>
+                        <div className='flex gap-3'>
+                            <div className='text-[#F57C00] font-semibold text-lg font-syne'>03</div>
+                            <h2 className='font-semibold font-syne text-lg'>SaaS</h2>
+                        </div>
+                        <p className='text-[#121820]/40 font-syne text-[15px] leading-[25.5px] mt-[30px]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, <br />
+                        sed do eiusmod tempor incididunt ut labore et dolore <br />
+                        magna aliqua.</p>
+                    </div>
+                </div>
+
+                <div className='flex gap-[121px]'>
+                    <img src={Save} alt="" className='w-[45px] h-[45px] ml-[31px]'/>
+                    <div>
+                        <div className='flex gap-3'>
+                            <div className='text-[#F57C00] font-semibold text-lg font-syne'>03</div>
+                            <h2 className='font-semibold font-syne text-lg'>Internet of Things</h2>
+                        </div>
+                        <p className='text-[#121820]/40 font-syne text-[15px] leading-[25.5px] mt-[30px]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, <br />
+                        sed do eiusmod tempor incididunt ut labore et dolore <br />
+                        magna aliqua.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className='w-[69%] flex mx-auto mt-[90px] pb-3 gap-1'>
+            <div className='w-[2%] h-[2px] bg-[#F57C00]'></div>
+            <div className='w-[96%] h-[2px] bg-[#121820]/10'></div>
+            <div className='w-[2%] h-[2px] bg-[#F57C00]'></div>
+        </div>
+        <div className='w-[10%] mx-auto flex justify-end'>
+            <img src={Shades} alt="" className='w-[203px] h-[140px]'/>
+        </div>
+        <div className='w-[69%] mx-auto'>
+            <div className='flex justify-between'>
+                <h1 className='font-syne font-semibold text-[42px]'>Latest Projects</h1>
+                <div className='flex gap-[30px]'>
+                    <button className='font-syne text-base font-semibold hover:text-[#121820]/50'>🡸    Prev</button>
+                    <button className='font-syne text-base font-semibold hover:text-[#121820]/50'>Next 🡺</button>
+                </div>
+            </div>
+        </div>
+        <div className='flex justify-end gap-[30px] mt-[90px]'>
+            <div>
+                <img src={Motion} alt="" className='w-[655px] h-[393px] rounded-lg'/>
+                <div className='flex gap-[85px] mt-7'>
+                    <div>
+                        <h2 className='font-syne font-semibold text-[22px]'>Easy & Most Powerful <br />
+                        Server Platform.</h2>
+                        <p className='font-syne font-semibold mt-[19px] text-base'>by: <span className='text-[#F57C00]'>Jane Meldrum</span></p>
+                    </div>
+                    <p className='font-syne leading-[25px] text-[#121820]/40'>Lorem ipsum dolor sit amet, consectetur <br />
+                        adipiscing elit, sed do eiusmod tempor <br />
+                        incididunt ut labore et dolore magna <br />
+                    aliqua.</p>
+                </div>
+            </div>
+
+            <div>
+                <img src={Phones} alt="" className='w-[655px] h-[393px] rounded-lg'/>
+                <div className='flex gap-[85px] mt-7'>
+                    <div>
+                        <h2 className='font-syne font-semibold text-[22px]'>Easy & Most Powerful <br />
+                        Server Platform.</h2>
+                        <p className='font-syne font-semibold mt-[19px] text-base'>by: <span className='text-[#F57C00]'>Nguta Ithya</span></p>
+                    </div>
+                    <p className='font-syne leading-[25px] text-[#121820]/40'>Lorem ipsum dolor sit amet, consectetur <br />
+                        adipiscing elit, sed do eiusmod tempor <br />
+                        incididunt ut labore et dolore magna <br />
+                    aliqua.</p>
+                </div>
+            </div>
+            <div className='mt-[420px]'>
+                <h2 className='font-syne font-semibold text-[22px]'>Easy & Most Powerful <br />
+                    Server Platform.</h2>
+                <p className='font-syne font-semibold mt-[19px] text-base'>by: <span className='text-[#F57C00]'>Roy Bricks</span></p>
+            </div>
+        </div>
+        <div className='w-[69%] mx-auto justify-between flex mt-[107px] mb-[120px]'>
+            <div className='flex gap-[14px] items-center group'>
+                <button className='font-syne font-semibold text-base group-hover:text-[#F57C00] '>View All Cases</button>
+                <button className='w-[35px] h-[35px] rounded-full text-white bg-[#F57C00] group-hover:bg-black'>🡺</button>
+            </div>
+            <button className='font-sans text-base py-5 px-14 border-[2px] border-[#F57C00] hover:bg-[#F57C00] rounded-full hover:text-white'>Get Started</button>
+        </div>
+        </>
+    )
+}
+export default Sec2
--- src/home.jsx
+++ src/home.jsx
@@ -1,8 +1,10 @@
 import Sec1 from './components/sec1'
+import Sec2 from './components/sec2'
 function Home() {
     return (
         <>
         <Sec1/>
+        <Sec2/>
         </>
     )
 } 
