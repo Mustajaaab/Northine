f28ef68fcf21a18de0227f46dcf0f283dcd43844
Mustajab <mustajabnabi@gmail.com>
2024-12-10 15:50:40 +0500

    Case Studies Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import './App.css';
-import ReactDOM from 'react-dom';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Team from './pages/Team';
 import About from './pages/About';
+import Services from './pages/Services';
+import Casestudies from './pages/casestudies';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
       <Route path="/team" element={<Team />} />
       <Route path="/about" element={<About />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/case-studies" element={<Casestudies />} />
     </Routes>
   );
 }
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,6 +1,8 @@
 function Services() {
     return (
-        <>love is life</>
+        <>
+        <h1 className="text-5xl flex items-center justify-center mt-44 font-syne text-stone-700">love is life</h1>
+        </>
     )
 }
 
