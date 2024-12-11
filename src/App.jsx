ef0cf184a812f411fe59f18e5e71d6f58f959cb0
zain <dev.zainulabbdin@gmail.com>
2024-12-11 11:09:21 +0500

    update routes and fix the page route issue

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,7 @@ import Home from './pages/Home';
 import Team from './pages/Team';
 import About from './pages/About';
 import Services from './pages/Services';
-<<<<<<< HEAD
-import Casestudies from './pages/casestudies';
-=======
->>>>>>> 3de220013904a85eddd0f68a1180a38c574969bf
+import Casestudies from './pages/Casestudies';
 
 function App() {
   return (
@@ -16,10 +13,7 @@ function App() {
       <Route path="/team" element={<Team />} />
       <Route path="/about" element={<About />} />
       <Route path="/services" element={<Services />} />
-<<<<<<< HEAD
       <Route path="/case-studies" element={<Casestudies />} />
-=======
->>>>>>> 3de220013904a85eddd0f68a1180a38c574969bf
     </Routes>
   );
 }
