62e1bad42acdc995ab15b20afc4502bb4db1581c
Mustajab <mustajabnabi@gmail.com>
2024-12-11 18:52:25 +0500

    service page update

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,18 @@ import Home from './pages/Home';
 import Team from './pages/Team';
 import About from './pages/About';
 import Services from './pages/Services';
-<<<<<<< HEAD
 import Casestudies from './pages/casestudies';
-=======
->>>>>>> 3de220013904a85eddd0f68a1180a38c574969bf
+import Contact from './pages/Contact-Us'
 
 function App() {
   return (
     <Routes>
-      <Route path="/home" element={<Home />} />
+      <Route path="/" element={<Home />} />
       <Route path="/team" element={<Team />} />
       <Route path="/about" element={<About />} />
       <Route path="/services" element={<Services />} />
-<<<<<<< HEAD
       <Route path="/case-studies" element={<Casestudies />} />
-=======
->>>>>>> 3de220013904a85eddd0f68a1180a38c574969bf
+      <Route path="/Contact-Us" element={<Contact />} />
     </Routes>
   );
 }
