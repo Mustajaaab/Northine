0dc8d961fda5ce10af1ddf41c51ce3ea392f3176
Rahat-Karim <karimrahat33@gmail.com>
2024-12-24 13:18:01 +0500

    model building Using a sequence-to-sequence model with an encoder-decoder architecture.

--- src/chatbot/chatbot.py
+++ src/chatbot/chatbot.py
@@ -7,6 +7,8 @@ from tqdm import tqdm
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from sklearn.model_selection import train_test_split
+from tensorflow.keras.layers import Embedding, LSTM, Dense, Input
+from tensorflow.keras.models import Model
 
 # TPU Setup
 try:
@@ -85,3 +87,22 @@ target_sequences = pad_sequences(target_sequences, maxlen=max_target_len, paddin
 input_train, input_val, target_train, target_val = train_test_split(
     input_sequences, target_sequences, test_size=0.2, random_state=42
 )
+# Encoder
+encoder_inputs = Input(shape=(max_input_len,))
+encoder_embedding = Embedding(input_vocab_size, LATENT_DIM)(encoder_inputs)
+encoder_lstm, state_h, state_c = LSTM(LATENT_DIM, return_state=True)(encoder_embedding)
+
+# Decoder
+decoder_inputs = Input(shape=(max_target_len,))
+decoder_embedding = Embedding(target_vocab_size, LATENT_DIM)(decoder_inputs)
+decoder_lstm = LSTM(LATENT_DIM, return_sequences=True, return_state=True)
+decoder_outputs, _, _ = decoder_lstm(decoder_embedding, initial_state=[state_h, state_c])
+decoder_dense = Dense(target_vocab_size, activation='softmax')
+decoder_outputs = decoder_dense(decoder_outputs)
+
+# Define the model
+model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
+
+# Compile the model
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.summary()
