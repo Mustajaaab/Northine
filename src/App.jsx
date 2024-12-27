c44025c20a03c4a70450c7bea7c39d9e141c608f
zain <dev.zainulabbdin@gmail.com>
2024-12-27 16:27:34 +0500

    fix the route isssue

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,18 @@ import Team from './pages/Team';
 import About from './pages/About';
 import Services from './pages/Services';
 import Casestudies from './pages/casestudies';
-import Contact from './pages/Contact-Us'
+import Contact from './pages/Contact-Us';
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="/home" element={<Home />} />
       <Route path="/team" element={<Team />} />
       <Route path="/about" element={<About />} />
       <Route path="/services" element={<Services />} />
       <Route path="/case-studies" element={<Casestudies />} />
-      <Route path="/Contact-Us" element={<Contact />} />
+      <Route path="/contact-us" element={<Contact />} />
     </Routes>
   );
 }
